fc24b1d3fab739db495eba26a8470e5d28d8b47b
Mohammad Sagor Ali <sagorbbr1@gmail.com>
2026-07-11 01:45:30 +0600

    scroll to top

--- client/src/layouts/MainLayout.jsx
+++ client/src/layouts/MainLayout.jsx
@@ -2,10 +2,12 @@ import { Outlet } from "react-router-dom";
 
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
+import ScrollToTop from "../components/ScrollToTop/ScrollToTop";
 
 const MainLayout = () => {
   return (
     <div className="pt-36">
+          <ScrollToTop />
       <Navbar />
 
       <Outlet />
